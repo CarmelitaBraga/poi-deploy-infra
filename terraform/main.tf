09089bdb67ce439eef60f238bc87bc0572c528c1
CarmelitaBraga <carmelitabraga8@gmail.com>
2024-10-06 15:23:06 -0300

    chore: adjusting according to standard

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,36 +45,36 @@ data "openstack_lb_loadbalancer_v2" "poi_atividades" {
   name = "poi-atividades"
 }
 
-# HTTP Listener for Load Balancer
-resource "openstack_lb_listener_v2" "poiderosa_http_listener" {
-  name            = "poiderosa-http-listener"
+# TCP Listener for Load Balancer
+resource "openstack_lb_listener_v2" "poiderosa_tcp_listener" {
+  name            = "poiderosa-tcp-listener"
   protocol        = "TCP"
   protocol_port   = 25180
   loadbalancer_id = data.openstack_lb_loadbalancer_v2.poi_atividades.id
 }
 
-# Pool for HTTP traffic
-resource "openstack_lb_pool_v2" "poiderosa_http_pool" {
-  name        = "poiderosas-projeto-http-pool"
+# Pool for TCP traffic
+resource "openstack_lb_pool_v2" "poiderosa_tcp_pool" {
+  name        = "poiderosas-projeto-tcp-pool"
   protocol    = "TCP"
   lb_method   = "ROUND_ROBIN"
-  listener_id = openstack_lb_listener_v2.poiderosa_http_listener.id
+  listener_id = openstack_lb_listener_v2.poiderosa_tcp_listener.id
 }
 
 # Associate Workers as Pool Members
-resource "openstack_lb_member_v2" "poiderosa_http_members" {
-  count          = 3
-  pool_id        = openstack_lb_pool_v2.poiderosa_http_pool.id
+resource "openstack_lb_member_v2" "poiderosa_tcp_members" {
+  count          = 2
+  pool_id        = openstack_lb_pool_v2.poiderosa_tcp_pool.id
   address        = element([openstack_compute_instance_v2.poiderosa_k8s_worker[0].access_ip_v4, openstack_compute_instance_v2.poiderosa_k8s_worker[1].access_ip_v4], count.index)
   protocol_port  = 80
   subnet_id      = "17de9c72-e5dc-4da0-ae0a-013f7e42400e"
 }
 
-# Security Group Rule for SSH (adjusted ports)
+# Security Group Rule for SSH
 resource "openstack_networking_secgroup_rule_v2" "allow_ssh" {
   direction         = "ingress"
   ethertype         = "IPv4"
-  protocol          = "tcp"
+  protocol          = "TCP"
   port_range_min    = 22
   port_range_max    = 22
   remote_ip_prefix  = "0.0.0.0/0"
@@ -151,4 +151,26 @@ resource "openstack_lb_member_v2" "poiderosa_ssh_member_worker_2" {
   address        = openstack_compute_instance_v2.poiderosa_k8s_worker[1].access_ip_v4
   protocol_port  = 22
   subnet_id      = "17de9c72-e5dc-4da0-ae0a-013f7e42400e"
+}
+
+# Allow Kubernetes API access (Port 6443)
+resource "openstack_networking_secgroup_rule_v2" "allow_k8s_api" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.default.id
+}
+
+# Allow HTTP traffic (Port 80)
+resource "openstack_networking_secgroup_rule_v2" "allow_http" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.default.id
 }
